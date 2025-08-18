ccada43aa09d5b2c1f8b2cad3efdfd787b36792d
Polina Panova <panovamoze@gmail.com>
2025-08-18 12:56:08 +0200

    qq

---
+++ Models/GuessingGameModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCAssignment_1___2.Models
+{
+    public class GuessingGameModel
+    {
+        public int ChanceCount { get; set; }
+        public int SecretNumber { get; set; }
+        public int GuessedNumber { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
