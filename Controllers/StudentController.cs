b038a2edfc47e664ec9c9c2d3893ecd4443aeded
Polina Panova <panovamoze@gmail.com>
2025-08-19 13:00:59 +0200

    Added student list

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MVCAssignment_1___2;
+using MVCAssignment_1___2.Models;
+
+namespace MVCAssignment_1___2.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student(1,"Nalini P", "12354", "nalini@gmail.com"), 
+            new Student(1,"Pipi L", "558877", "pippi@gmail.com"), 
+            new Student(1,"Johan K", "25368", "johan@gmail.com"), 
+            new Student(1,"Iggi O", "897463", "iggi@gmail.com"), 
+
+
+
+        };
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCAssignment_1___2.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public Student()
+        {
+
+        }
+         public Student(int sid, string name, string phone, string email)
+         {
+            SId = sid;
+            Name = name;
+            Phone = phone;
+            Email = email;
+
+         }
+
+
+    }
+}
